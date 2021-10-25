fcd7cb1f241b1461ac82ae4d9914c2524088f29c
Rome <rogeto20@jcu.edu>
2021-10-25 19:49:26 -0400

    Added Necessary Layers of the project

---
+++ InventoryProject.Service/Product/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using InventoryProject.Abstractions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryProject.Service.Product
+{
+    public interface IProductService
+    {
+        ProductDomainModel GetProductById(int id);
+    }
+}
---
+++ InventoryProject.Service/Product/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using InventoryProject.Abstractions.Models;
+using InventoryProject.Repository.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryProject.Service.Product
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(
+            IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public ProductDomainModel GetProductById(int id)
+        {
+            return new ProductDomainModel { ProductId = 5, ProductName = "Medicine", QuantityInStock = 10, BuyingPrice = 200 };
+        }
+    }
+}
